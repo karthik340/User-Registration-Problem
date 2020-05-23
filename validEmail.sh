a12f29e41fb0ad0f7d87396fff441e079f956268
karthik <nallaboluk@gmail.com>
2020-05-23 16:24:04 +0530

    adding enterValidEmailUC

---
+++ validEmail.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+echo enter valid email
+read email
+
+pattern="^[0-9a-zA-Z]+([+_-.][0-9a-zA-Z]+)?@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][0-9a-zA-Z]{2})?$"
+
+if [[ $email =~ $pattern ]]
+then
+     echo YES
+else
+     echo NO
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
